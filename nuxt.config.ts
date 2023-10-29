2b2f70ef79d4569ce10d05a2195cec25acbab869
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-10-30 06:47:32 +0900

    不要設定削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['/assets/css/style.css'],
-  storybook: {
-    addons: [],
-    stories: ['~/components/stories/**/*.stories.js'],
-    port: 6006,
-    parameters: {
-      backgrounds: {
-        default: 'light', // light or dark
-      },
-      controls: {
-        expanded: true,
-      },
-      layout: 'centered',
-    },
-  },
 })
