5de9389641d446f4330b4d7bfc756bf531e7bf8d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-11-04 10:10:58 +0900

    fetchテスト用ファイル作成

---
+++ pages/text.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+const { data: books } = await useFetch<{
+  books: { id: number; title: string; author: string }[]
+}>('/api/test')
+</script>
+
+<template>
+  <main>
+    <h1>Books</h1>
+    <ul>
+      <li v-for="{ id, title, author } of books?.books" :key="id">
+        {{ title }} ({{ author }})
+      </li>
+    </ul>
+  </main>
+</template>
