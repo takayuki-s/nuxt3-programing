477100350e7dc3c4cf5110eb92906a422762734a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-11-06 06:39:52 +0900

    messageAPI（エラー）

---
+++ pages/test.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+
+const fetchData = await useFetch('/message')
+console.log(fetchData)
+const message = fetchData.data.message
+</script>
+
+<template>
+  <main>
+    <h1>message</h1>
+    <p>{{ message }}</p>
+  </main>
+</template>
