65050929776d9ce982be536ab2639c2f11952486
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-11-10 14:50:21 +0900

    エラー中

--- pages/test.vue
+++ pages/test.vue
@@ -2,13 +2,13 @@
 const config = useRuntimeConfig()
 
 const fetchData = await useFetch('/message')
-console.log(fetchData.data.value)
-const message = fetchData.data.value.message
+console.log(fetchData.data)
+// const message = fetchData.data.value.message
 </script>
 
 <template>
   <main>
     <h1>message</h1>
-    <p>{{ message }}</p>
+    <!-- <p>{{ message }}</p> -->
   </main>
 </template>
