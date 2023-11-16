299da7f52e46072cfa49edec345fe18e20a0a86e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-11-16 09:56:19 +0900

    WP rest API アクセステスト

---
+++ pages/wp.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { data, pending, error } = await useFetch(
+  'http://localhost:8000/wp-json/wp/v2/posts/1',
+  { server: false },
+)
+console.log(data)
+</script>
+
+<template>
+  <div>
+    <template v-if="pending"> <p>loading...</p></template>
+    <template v-else-if="error">
+      <p>{{ error }}</p></template
+    >
+    <template v-else>
+      <h2>{{ data }}</h2>
+    </template>
+  </div>
+</template>
