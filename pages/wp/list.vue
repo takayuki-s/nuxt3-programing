9da79525d51faa5c277adf22c85d0238ba8718d8
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-11-17 08:33:23 +0900

    コンポーネント切り分け

--- pages/wp/list.vue
+++ pages/wp/list.vue
@@ -1,21 +1,7 @@
 <script setup lang="ts">
 import postList from '@/components/Organisms/PostList/index.vue'
-const { data, pending, error } = await useFetch(
-  'http://localhost:8000/wp-json/wp/v2/posts',
-  { server: false },
-)
-console.log(data)
 </script>
 
 <template>
-  <div>
-    <template v-if="pending"> <p>loading...</p></template>
-    <template v-else-if="error">
-      <p>{{ error }}</p></template
-    >
-    <template v-else>
-      <h2>Data</h2>
-      <div>{{ data }}</div>
-    </template>
-  </div>
+  <postList />
 </template>
