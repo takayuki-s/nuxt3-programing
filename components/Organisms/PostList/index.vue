219f9a1f0ec0aaaec4b87816ca6205d74b56f085
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-11-18 08:40:43 +0900

    コード整理

--- components/Organisms/PostList/index.vue
+++ components/Organisms/PostList/index.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-type ReturnFetchType<T extends string> = ReturnType<
-  typeof useFetch<void, unknown, T>
->['data']
+type ReturnFetchType<T> = ReturnType<typeof useFetch<void, unknown, T>>['data']
 const responseData: ReturnFetchType = await useFetch(
   'http://localhost:8000/wp-json/wp/v2/posts',
   {
     server: false,
   },
 ).data
-console.log(responseData)
 </script>
 
 <template>
