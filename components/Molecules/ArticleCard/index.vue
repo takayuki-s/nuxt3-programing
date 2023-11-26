96df6a38217ccaac6643fba8ae17baced777d9b9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-11-27 06:37:35 +0900

    データの受け渡し再度整理

--- components/Molecules/ArticleCard/index.vue
+++ components/Molecules/ArticleCard/index.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-const props = withDefaults(defineProps(), {
-  msg: 'hello',
-  labels: () => ['one', 'two'],
-})
-console.log(msg)
+import { ArticleCardData } from '@/types/article'
+type Props = {
+  data: ArticleCardData
+}
+const props = defineProps<Props>()
+console.log('props', props.data)
 </script>
 
 <template>
   <div
     class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
   >
-    <div>hello{{ msg }}</div>
+    <div>hello</div>
   </div>
 </template>
--- pages/wp/list.vue
+++ pages/wp/list.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts">
 import postList from '@/components/Organisms/PostList/index.vue'
-const { data, pending, error } = await useFetch(
-  'http://localhost:8000/wp-json/wp/v2/posts',
-  {
-    server: false,
-  },
-)
-const dataList = computed(() => {
-  return data
-})
 </script>
 
 <template>
-  <postList :dataList="dataList" test="test" />
+  <postList />
 </template>
