fe86224fdec72916191557d5a3e2e76abc15265c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-12-13 06:57:41 +0900

    vue-dompurify-html設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  plugins: [{ src: '@/plugins/mock', mode: 'client' }],
+  plugins: [
+    { src: '@/plugins/mock', mode: 'client' },
+    { src: '@/plugins/vue-dompurify-html', mode: 'client' },
+  ],
   css: ['/assets/css/style.css'],
   modules: ['@nuxtjs/tailwindcss'],
 })
