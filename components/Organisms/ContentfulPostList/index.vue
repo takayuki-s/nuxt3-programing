64e67c0f2cf7c4001e4ff583f68d9a4d2a1b5b99
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-12-18 06:30:48 +0900

    contentfulから記事一覧取得

--- components/Organisms/ContentfulPostList/index.vue
+++ components/Organisms/ContentfulPostList/index.vue
@@ -1,8 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+type ReturnFetchType<T extends string> = ReturnType<
+  typeof useFetch<void, unknown, T>
+>['data']
+const spaceId: string = import.meta.env.VITE_CONTENTFUL_SPACE_ID
+const environmentId: string = import.meta.env.VITE_CONTENTFUL_ENVIRONMENT_ID
+const accessToken: string = import.meta.env.VITE_CONTENTFUL_CD_ACCESS_TOKEN
+const {
+  data: postList,
+  pending,
+  error,
+} = await useFetch<ReturnFetchType<'https://cdn.contentful.com/spaces/'>>(
+  `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`,
+  {
+    server: false,
+  },
+)
+console.log(postList)
+</script>
 
 <template>
   <div>
-    <!-- <div v-if="pending">読み込み中です</div>
+    <div v-if="pending">読み込み中です</div>
     <div v-else-if="error">エラーが発生しました</div>
     <div v-else>
       <div
@@ -10,11 +28,12 @@
         class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 p-6"
       >
         <ul>
-          <div></div>
+          <div>
+            {{ postList }}
+          </div>
         </ul>
       </div>
       <div v-else>投稿データはありません</div>
-    </div> -->
-    contentful post list component
+    </div>
   </div>
 </template>
