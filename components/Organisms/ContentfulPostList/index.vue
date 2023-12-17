7724bf277c944ffd19efe3709a07307103289bf9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-12-17 09:06:42 +0900

    contentfulの記事一覧仮ページ

---
+++ components/Organisms/ContentfulPostList/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <!-- <div v-if="pending">読み込み中です</div>
+    <div v-else-if="error">エラーが発生しました</div>
+    <div v-else>
+      <div
+        v-if="postList"
+        class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 p-6"
+      >
+        <ul>
+          <div></div>
+        </ul>
+      </div>
+      <div v-else>投稿データはありません</div>
+    </div> -->
+    contentful post list component
+  </div>
+</template>
---
+++ pages/contentful/list.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import postList from '@/components/Organisms/ContentfulPostList/index.vue'
+</script>
+
+<template>
+  <postList />
+</template>
