9ec735b56aca23fa0b3c3628b998a88867262801
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-12-19 06:03:20 +0900

    戻り値の型定義

--- components/Organisms/ContentfulPostList/index.vue
+++ components/Organisms/ContentfulPostList/index.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
-type ReturnFetchType<T extends string> = ReturnType<
-  typeof useFetch<void, unknown, T>
->['data']
+type AdditionalProperties = {
+  [key: string]: any
+}
+type PostData<V extends AdditionalProperties> = {
+  items: {}
+  additionalProperties: V
+}
 const spaceId: string = import.meta.env.VITE_CONTENTFUL_SPACE_ID
 const environmentId: string = import.meta.env.VITE_CONTENTFUL_ENVIRONMENT_ID
 const accessToken: string = import.meta.env.VITE_CONTENTFUL_CD_ACCESS_TOKEN
@@ -9,13 +13,13 @@ const {
   data: postList,
   pending,
   error,
-} = await useFetch<ReturnFetchType<'https://cdn.contentful.com/spaces/'>>(
+} = await useFetch<PostData<AdditionalProperties>>(
   `https://cdn.contentful.com/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`,
   {
     server: false,
   },
 )
-console.log(postList)
+console.log(postList, typeof postList)
 </script>
 
 <template>
@@ -27,10 +31,10 @@ console.log(postList)
         v-if="postList"
         class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 p-6"
       >
-        <ul>
-          <div>
-            {{ postList }}
-          </div>
+        <ul v-for="(post, index) in postList.items" :key="index">
+          {{
+            post
+          }}
         </ul>
       </div>
       <div v-else>投稿データはありません</div>
