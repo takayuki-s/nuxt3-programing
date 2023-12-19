baab58a7f330ab7e004f47876e3c0f2959846750
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-12-20 06:57:41 +0900

    型変更、データ抽出中

--- components/Organisms/ContentfulPostList/index.vue
+++ components/Organisms/ContentfulPostList/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import ArticleCard from '@/components/Molecules/ArticleCard/index.vue'
+import { ArticleCardData } from '@/types/article'
 type AdditionalProperties = {
   [key: string]: any
 }
 type PostData<V extends AdditionalProperties> = {
   items: {}
+  includes: {
+    Asset: [{ sys: { id: string } }]
+    additionalProperties: V
+  }
   additionalProperties: V
 }
 const spaceId: string = import.meta.env.VITE_CONTENTFUL_SPACE_ID
@@ -20,6 +26,23 @@ const {
   },
 )
 console.log(postList, typeof postList)
+// console.log(postList.value?.includes.Asset)
+const filterData = (post: any) => {
+  console.log(postList.value?.includes.Asset[0].sys.id)
+  const filteredData: ArticleCardData = {
+    id: post.sys.id,
+    author: 0,
+    categories: [],
+    date: post.sys.createdAt,
+    modified: post.sys.updatedAt,
+    title: 'data.title.rendered',
+    link: 'data.link',
+    content: 'data.content',
+    excerpt: 'data.excerpt.rendered',
+    thumbnailUrl: '',
+  }
+  return filteredData
+}
 </script>
 
 <template>
@@ -32,6 +55,7 @@ console.log(postList, typeof postList)
         class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-5 p-6"
       >
         <ul v-for="(post, index) in postList.items" :key="index">
+          <ArticleCard :data="filterData(post)" />
           {{
             post
           }}
