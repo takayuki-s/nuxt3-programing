36a091e55ea369529b7fcb6df8ed619534272505
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-01-29 06:49:05 +0900

    Nabvar設置

---
+++ components/TailNavbar.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline'
+</script>
+
+<template>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="flex h-16 items-center justify-between">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img
+              class="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Your Company"
+            />
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+              <a
+                href="/"
+                class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                >TOP</a
+              >
+              <a
+                href="/contentful/list"
+                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                >LIST</a
+              >
+              <a
+                href="/about"
+                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                >ABOUT</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="-mr-2 flex sm:hidden">
+          <!-- Mobile menu button -->
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+        <DisclosureButton
+          as="a"
+          href="/"
+          class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+          >TOP</DisclosureButton
+        >
+        <DisclosureButton
+          as="a"
+          href="/contentful/list"
+          class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >LIST</DisclosureButton
+        >
+        <DisclosureButton
+          as="a"
+          href="/about"
+          class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+          >ABOUT</DisclosureButton
+        >
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import TailNavbar from '@/components/TailNavbar.vue'
+</script>
+
 <template>
   <div>
-    <!-- <Navbar /> -->
-    <slot />
+    <TailNavbar />
+    <div class="p-3">
+      <slot />
+    </div>
   </div>
 </template>
