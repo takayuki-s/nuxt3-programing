76ccbd6eba50b832b5d02afb03a99ab90acde996
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-02-05 07:01:28 +0900

    記事カードのNuxtLinkを廃止→クリックイベントに

--- components/Molecules/ArticleCard/index.vue
+++ components/Molecules/ArticleCard/index.vue
@@ -5,38 +5,45 @@ type Props = {
   data: ArticleCardData
 }
 const props = defineProps<Props>()
+
+const goDetailPage = () => {
+  navigateTo(`/contentful/post/${props.data.id}`, { external: true })
+}
+const filterCategory = (event: MouseEvent) => {
+  event.stopPropagation()
+}
 </script>
 
 <template>
-  <NuxtLink :to="`/contentful/post/${data.id}`">
-    <div
-      class="px-5 py-3 h-full bg-sky-50 rounded-xl shadow-md items-center cursor-pointer hover:bg-sky-100"
-    >
-      <img
-        v-if="data.thumbnailUrl"
-        class="object-cover h-48 w-full"
-        :src="data.thumbnailUrl"
-      />
-      <img
-        v-else
-        class="object-cover h-48 w-full"
-        src="@/assets/images/sample.jpg"
-      />
-      <div class="py-2 flex flex-col justify-between gap-1">
-        <p class="text-md font-bold">{{ data.title }}</p>
-        <p class="text-sm">{{ omitText(data.excerpt, 30) }}</p>
-        <p class="text-sm text-right">
-          <template v-for="tag in data.categories"
-            ><span
-              class="bg-green-100 z-10 hover:bg-green-200 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"
-              >{{ tag.sys.id }}</span
-            ></template
-          >
-        </p>
-        <p class="text-sm text-slate-400 text-right">
-          {{ formatDate(data.date) }}
-        </p>
-      </div>
+  <div
+    class="px-5 py-3 h-full bg-sky-50 rounded-xl shadow-md items-center cursor-pointer hover:bg-sky-100"
+    @click="goDetailPage"
+  >
+    <img
+      v-if="data.thumbnailUrl"
+      class="object-cover h-48 w-full"
+      :src="data.thumbnailUrl"
+    />
+    <img
+      v-else
+      class="object-cover h-48 w-full"
+      src="@/assets/images/sample.jpg"
+    />
+    <div class="py-2 flex flex-col justify-between gap-1">
+      <p class="text-md font-bold">{{ data.title }}</p>
+      <p class="text-sm">{{ omitText(data.excerpt, 30) }}</p>
+      <p class="text-sm text-right">
+        <template v-for="tag in data.categories"
+          ><span
+            class="bg-green-100 z-10 hover:bg-green-200 text-green-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300"
+            @click="filterCategory"
+            >{{ tag.sys.id }}</span
+          ></template
+        >
+      </p>
+      <p class="text-sm text-slate-400 text-right">
+        {{ formatDate(data.date) }}
+      </p>
     </div>
-  </NuxtLink>
+  </div>
 </template>
