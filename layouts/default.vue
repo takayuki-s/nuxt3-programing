721b14a164c3297497584546b94071fdbb3aba59
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-02-13 07:30:43 +0900

    フッターを下部固定

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,22 @@ import Footer from '@/components/Footer.vue'
 </script>
 
 <template>
-  <div>
+  <div class="layout-container">
     <TailNavbar />
     <div>
       <slot />
     </div>
-    <Footer />
+    <Footer class="footer" />
   </div>
 </template>
+
+<style scoped>
+.layout-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.footer {
+  margin-top: auto;
+}
+</style>
