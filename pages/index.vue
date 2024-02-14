b066357d557d7b6270f84033f73531c842abe14d
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-02-15 07:20:05 +0900

    トップ画像追加

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,18 @@ import Title from '@/components/Atoms/Title/index.vue'
 </script>
 
 <template>
+  <picture>
+    <img class="top-image" src="@/assets/images/top.jpg" />
+  </picture>
   <div class="p-5">
     <Title text="メインページ" />
   </div>
 </template>
+
+<style scoped>
+.top-image {
+  width: 100%;
+  height: 400px;
+  object-fit: cover;
+}
+</style>
