6d6ebc0d0dcb9aa8069c4d26aa10b78cb51ec1b0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-02-17 06:58:54 +0900

    フェードインテキストコンポーネント作成

---
+++ components/Atoms/FadeInText/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+type Props = {
+  text: string
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <p class="fade-in-text">{{ text }}</p>
+</template>
+
+<style scoped>
+.fade-in-text {
+  position: relative;
+  font-size: 60px;
+  opacity: 0;
+  transform: translateY(100%);
+  animation: fadeInFromBottom 3s forwards;
+}
+
+@keyframes fadeInFromBottom {
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
