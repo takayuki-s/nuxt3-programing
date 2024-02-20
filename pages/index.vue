6e4034a22735c7a5e75a7a9d49bc93b6ea5f033b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-02-21 07:59:25 +0900

    メインページに投稿一覧を

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
 import Title from '@/components/Atoms/Title/index.vue'
 import TopPicture from '@/components/Organisms/TopPicture/index.vue'
+import postList from '@/components/Organisms/ContentfulPostList/index.vue'
 </script>
 
 <template>
   <TopPicture />
-  <div class="p-5">
-    <Title text="メインページ" />
-  </div>
+  <postList />
 </template>
