c8776e608f44f059d9bae7cb1b6eda1559a8a448
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-02-24 09:07:01 +0900

    プロフィールカード仮作成

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 import Title from '@/components/Atoms/Title/index.vue'
 import TopPicture from '@/components/Organisms/TopPicture/index.vue'
 import postList from '@/components/Organisms/ContentfulPostList/index.vue'
+import ProfileCard from '@/components/Molecules/ProfileCard/index.vue'
 const limit = 10
 </script>
 
@@ -10,5 +11,6 @@ const limit = 10
   <postList :limit="limit" />
   <div class="p-5">
     <Title text="TEKE CAFEについて" />
+    <ProfileCard />
   </div>
 </template>
