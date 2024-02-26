bcac88111ce124aedaadb6ab94eb2b1c08d071f8
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-02-27 07:19:00 +0900

    同様にlist.vueでAPIフェッチ

--- pages/contentful/list.vue
+++ pages/contentful/list.vue
@@ -1,8 +1,37 @@
 <script setup lang="ts">
 import postList from '@/components/Organisms/ContentfulPostList/index.vue'
-const limit = 100
+import { createClient } from 'contentful'
+import { IBlogPageFields } from '~/@types/generated/contentful'
+
+type Tags = {
+  sys: {
+    id: string
+  }
+}
+type EntryItem = {
+  fields: IBlogPageFields
+  metadata: { tags: Tags[] }
+}
+
+const limit = 10
+
+const spaceId: string = import.meta.env.VITE_CONTENTFUL_SPACE_ID
+const accessToken: string = import.meta.env.VITE_CONTENTFUL_CD_ACCESS_TOKEN
+const client = createClient({
+  space: spaceId,
+  accessToken: accessToken,
+})
+const entries = await client.getEntries({
+  content_type: 'blogPage',
+  limit: limit,
+  order: ['-sys.createdAt'],
+})
+const entryItemList: EntryItem[] = []
+entries.items.forEach((entry) => {
+  entryItemList.push(entry)
+})
 </script>
 
 <template>
-  <postList :limit="limit" />
+  <postList :entryItemList="entryItemList" />
 </template>
