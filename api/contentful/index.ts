aa006b2a481f6153c1265116d8819535ed4f01d0
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-03-17 07:16:27 +0900

    API切り出し

---
+++ api/contentful/index.ts
@@ -0,0 +1,23 @@
+import { createClient } from 'contentful'
+
+const LIMIT: number = 10
+
+const spaceId: string = import.meta.env.VITE_CONTENTFUL_SPACE_ID
+const accessToken: string = import.meta.env.VITE_CONTENTFUL_CD_ACCESS_TOKEN
+const client = createClient({
+  space: spaceId,
+  accessToken: accessToken,
+})
+const entries = await client.getEntries({
+  content_type: 'blogPage',
+  limit: LIMIT,
+  order: ['-sys.createdAt'],
+})
+
+export const getEntriesByContentful = async () => {
+  return await client.getEntries({
+    content_type: 'blogPage',
+    limit: LIMIT,
+    order: ['-sys.createdAt'],
+  })
+}
--- pages/contentful/list.vue
+++ pages/contentful/list.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import postList from '@/components/Organisms/ContentfulPostList/index.vue'
-import { createClient } from 'contentful'
 import { IBlogPageFields } from '~/@types/generated/contentful'
+import { getEntriesByContentful } from '@/api/contentful'
 import Title from '@/components/Atoms/Title/index.vue'
 
 type Tags = {
@@ -14,19 +14,7 @@ type EntryItem = {
   metadata: { tags: Tags[] }
 }
 
-const limit = 10
-
-const spaceId: string = import.meta.env.VITE_CONTENTFUL_SPACE_ID
-const accessToken: string = import.meta.env.VITE_CONTENTFUL_CD_ACCESS_TOKEN
-const client = createClient({
-  space: spaceId,
-  accessToken: accessToken,
-})
-const entries = await client.getEntries({
-  content_type: 'blogPage',
-  limit: limit,
-  order: ['-sys.createdAt'],
-})
+const entries = await getEntriesByContentful()
 const entryItemList: EntryItem[] = []
 entries.items.forEach((entry) => {
   entryItemList.push(entry)
