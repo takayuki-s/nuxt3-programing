ac0c41e9140ca34a973f42f65c2fe4a268d8e74a
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-03-18 06:36:53 +0900

    トップページでも切り出したAPIを使用

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@ import Title from '@/components/Atoms/Title/index.vue'
 import TopPicture from '@/components/Organisms/TopPicture/index.vue'
 import postList from '@/components/Organisms/ContentfulPostList/index.vue'
 import ProfileCard from '@/components/Molecules/ProfileCard/index.vue'
-import { createClient } from 'contentful'
+import { getEntriesByContentful } from '@/api/contentful'
 import { IBlogPageFields } from '~/@types/generated/contentful'
 
 type Tags = {
@@ -16,19 +16,7 @@ type EntryItem = {
   metadata: { tags: Tags[] }
 }
 
-const limit = 10
-
-const spaceId: string = import.meta.env.VITE_CONTENTFUL_SPACE_ID
-const accessToken: string = import.meta.env.VITE_CONTENTFUL_CD_ACCESS_TOKEN
-const client = createClient({
-  space: spaceId,
-  accessToken: accessToken,
-})
-const entries = await client.getEntries({
-  content_type: 'blogPage',
-  limit: limit,
-  order: ['-sys.createdAt'],
-})
+const entries = await getEntriesByContentful()
 const entryItemList: EntryItem[] = []
 entries.items.forEach((entry) => {
   entryItemList.push(entry)
