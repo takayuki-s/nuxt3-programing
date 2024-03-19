a80ffc307687f326f4a94e0107f5dd447938d6e9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-03-20 08:40:32 +0900

    記事カードデザイン変更（ホバーアクション、上部にマステデザイン）

--- components/Molecules/ArticleCard/index.vue
+++ components/Molecules/ArticleCard/index.vue
@@ -54,23 +54,54 @@ const filterByTag = (id: string, event: MouseEvent) => {
 <style scoped>
 .article-card {
   position: relative;
-  border: 5px solid #eee; /* 線の太さ・種類・色 */
   z-index: 0;
+  border: 5px solid #ffffff;
   transition: all 0.4s;
 }
 .article-card:hover {
-  top: -3px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.3);
+  /* top: -3px; */
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
+  border: 5px solid #eee;
 }
-.article-card:before {
-  background-color: #cca497; /* ワインポイントの色 */
+.article-card:after {
+  width: 80px;
+  height: 35px;
+  background-color: #d9ccb3;
+  background-image: linear-gradient(
+      45deg,
+      #dfd4be 25%,
+      transparent 25%,
+      transparent 75%,
+      #dfd4be 75%,
+      #dfd4be
+    ),
+    linear-gradient(
+      45deg,
+      #dfd4be 25%,
+      transparent 25%,
+      transparent 75%,
+      #dfd4be 75%,
+      #dfd4be
+    );
+  background-position:
+    0 0,
+    12px 12px;
+  background-size: 24px 24px;
+  border-left: 2px dotted rgba(255, 255, 255, 0.9);
+  border-right: 2px dotted rgba(255, 255, 255, 0.9);
+  box-shadow: 0 0 5px rgba(255, 255, 255, 0.5);
   content: '';
   display: block;
+  margin: 0 0 10px 0;
+  padding: 5px 20px;
+  color: #fff; /* マステ部分文字色 */
+  text-align: center;
   position: absolute;
-  top: -5px;
-  left: -5px;
-  width: 5px;
-  height: 5px;
-  z-index: 1;
+  top: -25px;
+  left: calc(50% - 40px);
+  transform: rotate(-3deg);
+  -moz-transform: rotate(-3deg);
+  -webkit-transform: rotate(-3deg);
+  -o-transform: rotate(-3deg);
 }
 </style>
