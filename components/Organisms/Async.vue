bed7f5aeab6239e8556b2461d0c383146eb115a6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-03-26 08:29:15 +0900

    Suspense表示成功！

--- components/Organisms/Async.vue
+++ components/Organisms/Async.vue
@@ -1,5 +1,24 @@
 <script setup lang="ts">
 const { data, pending, error } = await useFetch('/message', { server: false })
+async function waitOneSecond() {
+  // Promiseを使って非同期処理を実行
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      // 1秒経過後にresolve()を呼び出す
+      resolve()
+    }, 1000) // 1000ミリ秒 = 1秒
+  })
+}
+
+// 非同期処理を呼び出し、待機する
+async function main() {
+  console.log('処理を開始します')
+  await waitOneSecond() // 1秒間待つ
+  console.log('1秒が経過しました')
+}
+
+// メイン関数を呼び出し
+await main()
 </script>
 
 <template>
