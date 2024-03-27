d099650ce39e9e41ebe75f69261cdb574112283c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-03-28 08:40:06 +0900

    テスト用で使ったコード削除

--- components/Organisms/Async.vue
+++ components/Organisms/Async.vue
@@ -1,18 +1,5 @@
 <script setup lang="ts">
 const { data, pending, error } = await useFetch('/message', { server: false })
-async function waitOneSecond() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('')
-    }, 1000)
-  })
-}
-
-async function main() {
-  await waitOneSecond()
-}
-
-await main()
 </script>
 
 <template>
