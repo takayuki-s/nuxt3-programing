c12336c6a72b934d02bb17baed3f7df90a8f188b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-04-02 06:31:55 +0900

    不要ファイル、コード削除

--- pages/test.vue
+++ pages/test.vue
@@ -1,20 +1,10 @@
 <script setup lang="ts">
 import Async from '@/components/Organisms/Async.vue'
-const { data, pending, error } = await useFetch('/message', { server: false })
 </script>
 
 <template>
   <main>
     <h1>message</h1>
-    <!-- <div>
-      <template v-if="pending"> <p>loading...</p></template>
-      <template v-else-if="error">
-        <p>{{ error }}</p></template
-      >
-      <template v-else>
-        <h2>{{ data.message }}</h2>
-      </template>
-    </div> -->
     <Suspense timeout="0">
       <template #default>
         <Async />
