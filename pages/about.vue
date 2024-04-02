181b0d82883cfba6b262b1284943fcef19db8d43
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-04-03 07:45:37 +0900

    aboutページ少し書きかけ

--- pages/about.vue
+++ pages/about.vue
@@ -5,5 +5,20 @@ import Title from '@/components/Atoms/Title/index.vue'
 <template>
   <div class="p-5">
     <Title text="ABOUTページ" />
+    <div class="flex justify-center p-5">
+      <div class="profile">
+        <p>こんにちは、てけです</p>
+        <p>自分の経歴を簡単に紹介します！</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.profile {
+  width: 620px;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+}
+</style>
