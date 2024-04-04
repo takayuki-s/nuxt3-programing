b9645e53df6795531745b27fca8be763b84dd10b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-04-05 07:12:18 +0900

    inputイベントテスト

--- pages/test.vue
+++ pages/test.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Async from '@/components/Organisms/Async.vue'
+const message = ref('')
+const updateMessage = (event: Event) => {
+  if (event.target instanceof HTMLInputElement) {
+    const inputValue = event.target.value.trim()
+    if (/^\d+$/.test(inputValue)) {
+      message.value = inputValue
+    } else {
+      message.value = ''
+    }
+  }
+}
 </script>
 
 <template>
-  <main>
+  <main class="p-5">
     <h1>message</h1>
     <Suspense timeout="0">
       <template #default>
@@ -13,5 +25,21 @@ import Async from '@/components/Organisms/Async.vue'
         <div>loading</div>
       </template>
     </Suspense>
+    <br />
+    <p>Message is: {{ message }}</p>
+    <input
+      class="text"
+      :value="message"
+      @input="(e) => updateMessage(e)"
+      placeholder=""
+    />
   </main>
 </template>
+
+<style scoped>
+.text {
+  height: 30px;
+  padding: 5px;
+  font-size: 16px;
+}
+</style>
