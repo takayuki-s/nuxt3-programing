fc819cabf6bb727cf7b13fd3ebbc3c7ff6c73724
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-04-07 07:49:43 +0900

    コンポーネント切り分け、読み込み

---
+++ components/Molecules/Input/InputNum.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const message = ref('')
+const updateMessage = (event: Event) => {
+  if (event.target instanceof HTMLInputElement) {
+    const inputValue = event.target.value.trim()
+    if (/^\d+$/.test(inputValue)) {
+      message.value = inputValue
+    } else {
+      const sanitizedValue = inputValue.replace(/[^\d]+/g, '')
+      event.target.value = sanitizedValue
+      message.value = sanitizedValue
+    }
+  }
+}
+</script>
+
+<template>
+  <p>Message is: {{ message }}</p>
+  <input
+    class="text"
+    :value="message"
+    @input="(e) => updateMessage(e)"
+    placeholder=""
+  />
+</template>
+
+<style scoped>
+.text {
+  height: 30px;
+  padding: 5px;
+  font-size: 16px;
+}
+</style>
--- pages/test.vue
+++ pages/test.vue
@@ -1,19 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import InputNum from '@/components/Molecules/input/InputNum.vue'
 import Async from '@/components/Organisms/Async.vue'
-const message = ref('')
-const updateMessage = (event: Event) => {
-  if (event.target instanceof HTMLInputElement) {
-    const inputValue = event.target.value.trim()
-    if (/^\d+$/.test(inputValue)) {
-      message.value = inputValue
-    } else {
-      const sanitizedValue = inputValue.replace(/[^\d]+/g, '')
-      event.target.value = sanitizedValue
-      message.value = sanitizedValue
-    }
-  }
-}
 </script>
 
 <template>
@@ -27,21 +14,8 @@ const updateMessage = (event: Event) => {
         <div>loading</div>
       </template>
     </Suspense>
-    <br />
-    <p>Message is: {{ message }}</p>
-    <input
-      class="text"
-      :value="message"
-      @input="(e) => updateMessage(e)"
-      placeholder=""
-    />
+    <InputNum />
   </main>
 </template>
 
-<style scoped>
-.text {
-  height: 30px;
-  padding: 5px;
-  font-size: 16px;
-}
-</style>
+<style scoped></style>
