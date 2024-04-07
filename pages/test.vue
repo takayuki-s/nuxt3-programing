19039897349dd31cbbbcc348087187a006ee9746
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-04-08 06:22:41 +0900

    import文修正

--- pages/test.vue
+++ pages/test.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import InputNum from '@/components/Molecules/input/InputNum.vue'
+import InputNum from '@/components/Molecules/Input/InputNum.vue'
 import Async from '@/components/Organisms/Async.vue'
 </script>
 
