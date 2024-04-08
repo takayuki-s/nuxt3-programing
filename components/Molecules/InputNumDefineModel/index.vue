ae8e8c86aad5ca1b62e47d28131c9bd2d94923f2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-04-09 07:23:58 +0900

    defineModelを使ったinputコンポーネント作成、読み込み

---
+++ components/Molecules/InputNumDefineModel/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const model = defineModel()
+</script>
+
+<template>
+  <input class="text" v-model="model" />
+</template>
+
+<style scoped>
+.text {
+  height: 30px;
+  padding: 5px;
+  font-size: 16px;
+}
+</style>
--- pages/test.vue
+++ pages/test.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import InputNum from '@/components/Molecules/InputNum/index.vue'
+import InputNumDefineModel from '@/components/Molecules/InputNumDefineModel/index.vue'
 import Async from '@/components/Organisms/Async.vue'
 
 /**
  * Refs
  */
 const value = ref<number | string>('')
+const valueWithDefineModel = ref<number | string>('')
 
 /**
  * Methods
@@ -28,11 +30,13 @@ const updateValue = (inputValue: number | string) => {
         <div>loading</div>
       </template>
     </Suspense>
-    <p>Parent component Message is: {{ value }}</p>
+    <p>Parent component value is: {{ value }}</p>
     <InputNum
       :value="value"
       @update-value="(inputValue) => updateValue(inputValue)"
     />
+    <p>Parent component valueWithDefineModel is: {{ valueWithDefineModel }}</p>
+    <InputNumDefineModel v-model="valueWithDefineModel" />
   </main>
 </template>
 
