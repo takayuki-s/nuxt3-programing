721c6cb7bee4a010e7e85473a22226a0d2127a24
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-04-11 07:06:57 +0900

    簡易バリデーション

---
+++ pages/validation.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useField } from 'vee-validate'
+import * as yup from 'yup'
+
+const { value, errorMessage } = useField(
+  'test',
+  yup.string().required('この項目は必須です'),
+)
+</script>
+
+<template>
+  <div class="p-5 validation-field">
+    <h1>バリデーションテスト（必須）</h1>
+    <input type="text" v-model="value" />
+    <p>{{ errorMessage }}</p>
+  </div>
+</template>
+
+<style scoped>
+.validation-field {
+  input {
+    height: 30px;
+    padding: 5px;
+    font-size: 16px;
+  }
+}
+</style>
