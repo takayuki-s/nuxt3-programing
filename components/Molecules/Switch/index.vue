ebd22ac9d7185c76eec70e042149abfddae2f134
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-05-29 05:47:37 +0900

    Switchコンポーネント作成

---
+++ components/Molecules/Switch/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { SwitchRoot, SwitchThumb } from 'radix-vue'
+import { ref } from 'vue'
+import './style.css'
+
+const switchState = ref(false)
+</script>
+
+<template>
+  <div :style="{ display: 'flex', alignItems: 'center' }">
+    <label class="Label" for="airplane-mode"> Airplane mode </label>
+    <SwitchRoot
+      id="airplane-mode"
+      v-model:checked="switchState"
+      class="SwitchRoot"
+    >
+      <SwitchThumb class="SwitchThumb" />
+    </SwitchRoot>
+  </div>
+</template>
