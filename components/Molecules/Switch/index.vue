19588fb0056e0dfb0810105f32c74e16b1bf4668
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-06-02 07:15:06 +0900

    スタイル調整

--- components/Molecules/Switch/index.vue
+++ components/Molecules/Switch/index.vue
@@ -26,10 +26,10 @@ button {
 .SwitchRoot {
   width: 42px;
   height: 25px;
-  background-color: var(--black-a9);
+  background-color: black;
   border-radius: 9999px;
   position: relative;
-  box-shadow: 0 2px 10px var(--black-a7);
+  box-shadow: 0 2px 10px black;
   -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
 .SwitchRoot:focus {
@@ -45,7 +45,7 @@ button {
   height: 21px;
   background-color: white;
   border-radius: 9999px;
-  box-shadow: 0 2px 2px var(--black-a7);
+  box-shadow: 0 2px 2px black;
   transition: transform 100ms;
   transform: translateX(2px);
   will-change: transform;
@@ -55,7 +55,7 @@ button {
 }
 
 .Label {
-  color: white;
+  color: black;
   font-size: 15px;
   line-height: 1;
 }
