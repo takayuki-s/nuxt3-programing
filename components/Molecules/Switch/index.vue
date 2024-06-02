778d01bd7140883929ec29188bec075bfdda4e44
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-06-03 07:43:35 +0900

    style.cssを読み込ませるように戻す

--- components/Molecules/Switch/index.vue
+++ components/Molecules/Switch/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { SwitchRoot, SwitchThumb } from 'radix-vue'
 import { ref } from 'vue'
+import './style.css'
 
 const switchState = ref(false)
 </script>
@@ -17,46 +18,3 @@ const switchState = ref(false)
     </SwitchRoot>
   </div>
 </template>
-
-<style scoped>
-button {
-  all: unset;
-}
-
-.SwitchRoot {
-  width: 42px;
-  height: 25px;
-  background-color: black;
-  border-radius: 9999px;
-  position: relative;
-  box-shadow: 0 2px 10px black;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-}
-.SwitchRoot:focus {
-  box-shadow: 0 0 0 2px black;
-}
-.SwitchRoot[data-state='checked'] {
-  background-color: black;
-}
-
-.SwitchThumb {
-  display: block;
-  width: 21px;
-  height: 21px;
-  background-color: white;
-  border-radius: 9999px;
-  box-shadow: 0 2px 2px black;
-  transition: transform 100ms;
-  transform: translateX(2px);
-  will-change: transform;
-}
-.SwitchThumb[data-state='checked'] {
-  transform: translateX(19px);
-}
-
-.Label {
-  color: black;
-  font-size: 15px;
-  line-height: 1;
-}
-</style>
