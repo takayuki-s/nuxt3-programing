ee570a16ecb3f1e259063e3b70ea5a4e63477f1c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-06-13 08:57:50 +0900

    slotつきinputフォームコンポーネント修正

--- components/Atoms/InputSlotForm/index.vue
+++ components/Atoms/InputSlotForm/index.vue
@@ -10,44 +10,17 @@ const props = withDefaults(defineProps<Props>(), {
   label: '',
   errorMessage: '',
 })
-
-/**
- * Emit
- */
-type Emits = {
-  (e: 'handleChange', value: unknown | string): void
-}
-const emit = defineEmits<Emits>()
-
-/**
- * defineModel
- */
-const modelValue = defineModel()
-
-const updateValue = () => {
-  emit('handleChange', modelValue.value)
-}
 </script>
 
 <template>
   <div class="flex flex-col">
     <label>{{ props.label }}</label>
-    <input
-      class="input"
-      v-model="modelValue"
-      @blur="updateValue"
-      @input="updateValue"
-    />
+    <slot />
     <p class="error-message">{{ props.errorMessage }}</p>
   </div>
 </template>
 
 <style scoped>
-.input {
-  background-color: white;
-  padding: 5px;
-  width: 300px;
-}
 .error-message {
   margin: 5px;
   font-size: 12px;
