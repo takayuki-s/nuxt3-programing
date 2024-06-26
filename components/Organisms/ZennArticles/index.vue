0e0f0dd4bc38ead27237033e4919d01faee0d92e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-06-27 07:32:06 +0900

    zenn記事表示用コンポーネント配置

---
+++ components/Organisms/ZennArticles/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { ZennArticle } from '@/types/zennArticles'
+type Props = {
+  articles: ZennArticle[]
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div>{{ articles }}</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 import Title from '@/components/Atoms/Title/index.vue'
 import TopPicture from '@/components/Organisms/TopPicture/index.vue'
 import postList from '@/components/Organisms/ContentfulPostList/index.vue'
+import ZennArticles from '@/components/Organisms/ZennArticles/index.vue'
 import ProfileCard from '@/components/Molecules/ProfileCard/index.vue'
 import { fetchEntriesByContentful } from '@/api/contentful'
 import { IBlogPageFields } from '~/@types/generated/contentful'
@@ -47,6 +48,10 @@ console.log(zennArticles)
     <Title text="TEKE CAFEについて" />
     <div class="p-5">
       <ProfileCard />
+    </div>
+    <Title text="Zenn 記事一覧" />
+    <div class="p-5">
+      <ZennArticles :articles="zennArticles" />
     </div>
   </div>
 </template>
