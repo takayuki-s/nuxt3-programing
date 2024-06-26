bf7bc47935d06854cb08d42fe96e891cb655f916
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-06-27 07:48:27 +0900

    記事一覧リンク

--- components/Organisms/ZennArticles/index.vue
+++ components/Organisms/ZennArticles/index.vue
@@ -4,8 +4,18 @@ type Props = {
   articles: ZennArticle[]
 }
 const props = defineProps<Props>()
+
+const getZennUrl = (path) => {
+  return 'https://zenn.dev' + path
+}
 </script>
 
 <template>
-  <div>{{ articles }}</div>
+  <p v-for="article in articles" :key="article.id">
+    <nuxt-link class="link" :to="getZennUrl(article.path)" target="_blank">{{
+      article.title
+    }}</nuxt-link>
+  </p>
 </template>
+
+<style scoped></style>
