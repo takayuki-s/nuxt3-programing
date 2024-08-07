7ebf5eda5bfec8a2609a36cb8c6448e97e5c9c64
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-08-08 08:26:30 +0900

    languageプルダウンのレスポンシブ対応

--- components/TailNavbar.vue
+++ components/TailNavbar.vue
@@ -77,11 +77,12 @@ const isValidation = computed(() => {
               </div>
             </div>
           </div>
-          <LanguageMenu />
+          <LanguageMenu class="invisible sm:visible" />
 
-          <div class="-mr-2 flex sm:hidden">
+          <div class="-mr-2 flex gap-2 sm:hidden">
             <!-- Mobile menu button -->
             <ClientOnly>
+              <LanguageMenu />
               <DisclosureButton
                 class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               >
