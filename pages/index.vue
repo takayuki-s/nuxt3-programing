d72e0b60d565fb0ead1829fc8c9e9b5b7ceef2a4
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-08-12 20:48:36 +0900

    zehnのAPI取得一旦停止

--- pages/index.vue
+++ pages/index.vue
@@ -29,9 +29,9 @@ const filteredEntryItemList = computed(() => {
     )
   })
 })
-const { data: zennArticles } = await useFetch(
-  import.meta.env.VITE_ZENN_ARTICLES_FETCH_API,
-)
+// const { data: zennArticles } = await useFetch(
+//   import.meta.env.VITE_ZENN_ARTICLES_FETCH_API,
+// )
 </script>
 
 <template>
