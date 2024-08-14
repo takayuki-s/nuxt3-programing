fc0359060e5b186f4fb17c934c0e941cc9001b70
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-08-15 05:40:19 +0900

    一旦コメントアウト解除

--- pages/index.vue
+++ pages/index.vue
@@ -29,13 +29,9 @@ const filteredEntryItemList = computed(() => {
     )
   })
 })
-// const { data: zennArticles } = await useFetch(
-//   import.meta.env.VITE_ZENN_ARTICLES_FETCH_API,
-// )
-// onMounted(async () => {
-//   const { data } = await useFetch(import.meta.env.VITE_ZENN_ARTICLES_FETCH_API)
-//   console.log(data)
-// })
+const { data: zennArticles } = await useFetch(
+  import.meta.env.VITE_ZENN_ARTICLES_FETCH_API,
+)
 </script>
 
 <template>
