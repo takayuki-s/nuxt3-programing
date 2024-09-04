4daeea8b1ee5c6171064e9b2acfec5bb1e7ee7c3
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-09-05 08:16:22 +0900

    aboutページタイトルも英語対応

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import Title from '@/components/Atoms/Title/index.vue'
+const { t } = useI18n()
 </script>
 
 <template>
   <div class="p-5">
-    <Title text="ABOUTページ" />
+    <Title :text="t('about.title')" />
     <div class="flex justify-center p-5">
       <div class="profile">
         <p>{{ $t('about.greeting_1') }}</p>
