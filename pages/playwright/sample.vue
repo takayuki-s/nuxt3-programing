594cd489e6ff323dabeb3b8304c60b940299f510
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-10-21 06:51:10 +0900

    テストページ作成

---
+++ pages/playwright/sample.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: '最初のページ',
+  meta: [{ name: '最初のステップ', content: 'My amazing site.' }],
+})
+</script>
+
+<template>
+  <main>
+    <h1>Playwright</h1>
+    <p>E2Eテスト用</p>
+    <p>
+      <button>ボタン</button>
+    </p>
+  </main>
+</template>
