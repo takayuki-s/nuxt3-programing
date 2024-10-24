c8d10d0ea9ca5d70b57a8ced8431791c19462d26
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-10-25 07:01:35 +0900

    操作テスト（入力3つver）

--- tests/form.spec.ts
+++ tests/form.spec.ts
@@ -9,3 +9,17 @@ test('フォーム操作のテスト', async ({ page }) => {
   await page.getByRole('button', { name: /シャッフル/ }).click()
   await expect(page.getByLabel('結果')).toHaveText(/A → B|B → A/)
 })
+
+test('フォーム操作のテスト（3つver）', async ({ page }) => {
+  await page.goto('http://localhost:3000/playwright/form')
+  await page.getByPlaceholder(/1人目の名前を入力/).click()
+  await page.getByPlaceholder(/1人目の名前を入力/).press('A')
+  await page.getByPlaceholder(/2人目の名前を入力/).click()
+  await page.getByPlaceholder(/2人目の名前を入力/).fill('B')
+  await page.getByPlaceholder(/3人目の名前を入力/).click()
+  await page.getByPlaceholder(/3人目の名前を入力/).fill('C')
+  await page.getByRole('button', { name: /シャッフル/ }).click()
+  await expect(page.getByLabel('結果')).toHaveText(
+    /A → B|B → A|A → C|C → A|C → B|B → C/,
+  )
+})
