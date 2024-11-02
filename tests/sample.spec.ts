be0bd690bd384de9f132ff3608a91e153cff924c
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2024-11-02 20:42:49 +0900

    テストケース調整テスト

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -4,6 +4,7 @@ test('ページ表示のテスト', async ({ page }) => {
   await page.goto('http://localhost:3000/playwright/sample')
   await expect(page).toHaveTitle('最初のページ')
   await expect(page.getByRole('heading')).toHaveText('Playwright')
+  await expect(page.getByRole('button')).toHaveText('ボタン')
   await expect(page.getByRole('button', { name: /ボタン/ })).toBeVisible()
 })
 
