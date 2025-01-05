14ee3e08219b32c9cc84f1e9e61fc946df52b83e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2025-01-06 07:00:18 +0900

    エラー時の表示NuxtErrorBoundaryで

--- pages/error/errorHandleBasic.vue
+++ pages/error/errorHandleBasic.vue
@@ -3,7 +3,13 @@ import ErrorGeneratorBasic from '@/components/Atoms/ErrorGeneratorBasic/index.vu
 </script>
 
 <template>
-  <ErrorGeneratorBasic />
+  <NuxtErrorBoundary>
+    <ErrorGeneratorBasic />
+    <template #error="errorProps">
+      <p>エラーが発生しました</p>
+      <p>{{ errorProps.error }}</p>
+    </template>
+  </NuxtErrorBoundary>
   <p>
     <NuxtLink to="/">戻る</NuxtLink>
   </p>
