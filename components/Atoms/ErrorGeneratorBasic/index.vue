f680f1095dd3dca69241d03f53643d415653f8aa
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2025-01-06 06:54:40 +0900

    エラー発生用コンポーネント作成

---
+++ components/Atoms/ErrorGeneratorBasic/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const onThrowsErrorClick = () => {
+  throw createError('エラー発生')
+}
+</script>
+
+<template>
+  <section>エラー<button @click="onThrowsErrorClick">発生</button></section>
+</template>
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+</style>
--- pages/error/errorHandleBasic.vue
+++ pages/error/errorHandleBasic.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import ErrorGeneratorBasic from '@/components/Atoms/ErrorGeneratorBasic/index.vue'
+</script>
+
 <template>
+  <ErrorGeneratorBasic />
   <p>
     <NuxtLink to="/">戻る</NuxtLink>
   </p>
