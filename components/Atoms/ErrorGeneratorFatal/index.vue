a28f8ee1bd9a7ce27f4d2a580b182da4b5fec397
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2025-01-07 07:03:56 +0900

    fatalエラー発生用のコンポーネント作成

---
+++ components/Atoms/ErrorGeneratorFatal/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const onThrowsErrorClick = () => {
+  throw createError({
+    message: 'fatalエラー発生',
+    fatal: true,
+  })
+}
+</script>
+
+<template>
+  <section>
+    fatalエラー<button @click="onThrowsErrorClick">発生</button>
+  </section>
+</template>
