e7f1323e5cc73c0a47fe37c3667b8234cf618164
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2025-01-07 06:53:02 +0900

    カスタムエラー画面

---
+++ pages/error.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+type Props = {
+  error: {
+    statusCode?: string
+    statusMessage?: string
+    message?: string
+    stack?: string
+  }
+}
+const props = defineProps<Props>()
+
+const onBackButtonClick = () => {
+  clearError({ redirect: '/' })
+}
+</script>
+
+<template>
+  <h1>エラーが発生しました</h1>
+  <p>{{ props.error.statusCode }}</p>
+  <p>{{ props.error.statusMessage }}</p>
+  <p>{{ props.error.message }}</p>
+  <p>{{ props.error.stack }}</p>
+  <button @click="onBackButtonClick">戻る</button>
+</template>
