f35c15512f08974229389e5e4ac641babd9fcecf
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2025-01-08 08:24:46 +0900

    fatalエラーコンポーネント読み込みページ

---
+++ pages/error/errorHandleFatal.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Ref } from 'vue'
+import ErrorGeneratorFatal from '@/components/Atoms/ErrorGeneratorFatal/index.vue'
+
+const onResetButtonClick = (error: Ref) => {
+  error.value = null
+}
+</script>
+
+<template>
+  <NuxtErrorBoundary>
+    <ErrorGeneratorFatal />
+    <template #error="errorProps">
+      <p>エラーが発生しました</p>
+      <p>{{ errorProps.error }}</p>
+      <button @click="onResetButtonClick(errorProps.error)">
+        エラーリセット
+      </button>
+    </template>
+  </NuxtErrorBoundary>
+  <p>
+    <NuxtLink to="/">戻る</NuxtLink>
+  </p>
+</template>
